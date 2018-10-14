2e53967334f022e3fb0d6b146cd7ebffbce6d818
Christophe SAUVEUR <christophe@cheeseburgames.com>
2018-10-14 23:56:13 +0200

    Renaming class back to its original name

--- Scripts/AnimatorStateMachineUtil.cs
+++ Scripts/AnimatorStateMachineUtil.cs
@@ -29,7 +29,7 @@ using System.Reflection;
 
 namespace AnimatorStateMachineUtil
 {
-    public class FSMUtil : MonoBehaviour
+    public class AnimatorStateMachineUtil : MonoBehaviour
     {
         public bool autoUpdate;
 
