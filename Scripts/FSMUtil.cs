8d30011b28f0b813fc412c7cb469b38cde6ac95c
Christophe SAUVEUR <christophe@cheeseburgames.com>
2018-10-14 09:06:46 +0200

    Removed Linq dependancy

--- Scripts/FSMUtil.cs
+++ Scripts/FSMUtil.cs
@@ -24,7 +24,6 @@ THE SOFTWARE.
 
 using UnityEngine;
 using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.Reflection;
 
@@ -49,12 +48,12 @@ namespace AnimatorStateMachineUtil
             }
         }
 
-        protected Animator _animator;
-        protected Lookup<int, Action> stateHashToUpdateMethod;
-        protected Lookup<int, Action> stateHashToEnterMethod;
-        protected Lookup<int, Action> stateHashToExitMethod;
-        protected Dictionary<int, string> hashToAnimString;
-        protected int[] _lastStateLayers;
+        private Animator _animator;
+        private Dictionary<int, List<Action>> stateHashToUpdateMethod = new Dictionary<int, List<Action>>();
+        private Dictionary<int, List<Action>> stateHashToEnterMethod = new Dictionary<int, List<Action>>();
+        private Dictionary<int, List<Action>> stateHashToExitMethod = new Dictionary<int, List<Action>>();
+        private Dictionary<int, string> hashToAnimString;
+        private int[] _lastStateLayers;
 
         void Awake() {
             _lastStateLayers = new int[Animator.layerCount];
@@ -72,30 +71,30 @@ namespace AnimatorStateMachineUtil
         }
 
         public void StateMachineUpdate() {
+            List<Action> actions;
+
             for (int layer = 0; layer < _lastStateLayers.Length; layer++) {
                 int _lastState = _lastStateLayers[layer];
                 int stateId = Animator.GetCurrentAnimatorStateInfo(layer).fullPathHash;
                 if (_lastState != stateId) {
 
-                    if (stateHashToExitMethod.Contains(_lastState)) {
-                        foreach (Action action in stateHashToExitMethod[_lastState]) {
+                    if (stateHashToExitMethod.TryGetValue(_lastState, out actions)) {
+                        foreach (Action action in actions) {
                             action.Invoke();
                         }
                     }
 
-                    if (stateHashToEnterMethod.Contains(stateId)) {
-                        foreach (Action action in stateHashToEnterMethod[stateId]) {
+                    if (stateHashToEnterMethod.TryGetValue(stateId, out actions)) {
+                        foreach (Action action in actions) {
                             action.Invoke();
                         }
-
                     }
                 }
 
-                if (stateHashToUpdateMethod.Contains(stateId)) {
-                    foreach (Action action in stateHashToUpdateMethod[stateId]) {
+                if (stateHashToUpdateMethod.TryGetValue(stateId, out actions)) {
+                    foreach (Action action in actions) {
                         action.Invoke();
                     }
-
                 }
 
                 _lastStateLayers[layer] = stateId;
@@ -104,102 +103,90 @@ namespace AnimatorStateMachineUtil
 
         void DiscoverStateMethods() {
             hashToAnimString = new Dictionary<int, string>();
-            var components = gameObject.GetComponents<MonoBehaviour>();
+            MonoBehaviour[] components = gameObject.GetComponents<MonoBehaviour>();
 
-            List<StateMethod> enterStateMethods = new List<StateMethod>();
-            List<StateMethod> updateStateMethods = new List<StateMethod>();
-            List<StateMethod> exitStateMethods = new List<StateMethod>();
+            stateHashToUpdateMethod.Clear();
+            stateHashToEnterMethod.Clear();
+            stateHashToExitMethod.Clear();
 
-            foreach (var component in components) {
-                if (component == null) continue;
+            foreach (MonoBehaviour component in components) {
+                if (component != null) {
+                    Type type = component.GetType();
+                    MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.InvokeMethod);
 
-                var type = component.GetType();
-                var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.InvokeMethod);
+                    foreach (MethodInfo method in methods) {
+                        object[] attributes;
 
-                foreach (var method in methods) {
-                    object[] attributes;
-
-                    attributes = method.GetCustomAttributes(typeof(StateUpdateMethod), true);
-                    foreach (StateUpdateMethod attribute in attributes) {
-                        var parameters = method.GetParameters();
-                        if (parameters.Length == 0) {
-                            updateStateMethods.Add(CreateStateMethod(attribute.state, method, component));
+                        attributes = method.GetCustomAttributes(typeof(StateUpdateMethodAttribute), true);
+                        foreach (StateUpdateMethodAttribute attribute in attributes) {
+                            ParameterInfo[] parameters = method.GetParameters();
+                            if (parameters.Length == 0) {
+                                AddStateMethod(stateHashToUpdateMethod, attribute.state, method, component);
+                            }
                         }
-                    }
 
-
-                    attributes = method.GetCustomAttributes(typeof(StateEnterMethod), true);
-                    foreach (StateEnterMethod attribute in attributes) {
-
-                        var parameters = method.GetParameters();
-                        if (parameters.Length == 0) {
-                            enterStateMethods.Add(CreateStateMethod(attribute.state, method, component));
-
+                        attributes = method.GetCustomAttributes(typeof(StateEnterMethodAttribute), true);
+                        foreach (StateEnterMethodAttribute attribute in attributes) {
+                            ParameterInfo[] parameters = method.GetParameters();
+                            if (parameters.Length == 0) {
+                                AddStateMethod(stateHashToEnterMethod, attribute.state, method, component);
+                            }
                         }
-                    }
 
-                    attributes = method.GetCustomAttributes(typeof(StateExitMethod), true);
-                    foreach (StateExitMethod attribute in attributes) {
-
-                        var parameters = method.GetParameters();
-                        if (parameters.Length == 0) {
-                            exitStateMethods.Add(CreateStateMethod(attribute.state, method, component));
-
+                        attributes = method.GetCustomAttributes(typeof(StateExitMethodAttribute), true);
+                        foreach (StateExitMethodAttribute attribute in attributes) {
+                            ParameterInfo[] parameters = method.GetParameters();
+                            if (parameters.Length == 0) {
+                                AddStateMethod(stateHashToExitMethod, attribute.state, method, component);
+                            }
                         }
                     }
                 }
             }
-
-            stateHashToUpdateMethod = (Lookup<int, Action>) updateStateMethods.ToLookup<StateMethod, int, Action>(p => p.stateHash, p => p.method);
-            stateHashToEnterMethod = (Lookup<int, Action>) enterStateMethods.ToLookup<StateMethod, int, Action>(p => p.stateHash, p => p.method);
-            stateHashToExitMethod = (Lookup<int, Action>) exitStateMethods.ToLookup<StateMethod, int, Action>(p => p.stateHash, p => p.method);
         }
 
-        StateMethod CreateStateMethod(string state, MethodInfo method, MonoBehaviour component) {
-            int stateHash = Animator.StringToHash(state);
-            hashToAnimString[stateHash] = state;
-            StateMethod stateMethod = new StateMethod();
-            stateMethod.stateHash = stateHash;
-            stateMethod.method = () => {
-                method.Invoke(component, null);
-            };
-            return stateMethod;
+        private void AddStateMethod(Dictionary<int, List<Action>> _dictionnary, string _state, MethodInfo _method, MonoBehaviour _component) {
+            int stateHash = Animator.StringToHash(_state);
+
+            List<Action> actions = null;
+            if (!_dictionnary.TryGetValue(stateHash, out actions)) {
+                actions = new List<Action>();
+                _dictionnary[stateHash] = actions;
+            }
+
+            actions.Add(() => {
+                _method.Invoke(_component, null);
+            });
         }
     }
 
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
-    public class StateUpdateMethod : System.Attribute
+    public class StateUpdateMethodAttribute : Attribute
     {
         public string state;
 
-        public StateUpdateMethod(string state) {
+        public StateUpdateMethodAttribute(string state) {
             this.state = state;
         }
     }
 
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
-    public class StateEnterMethod : System.Attribute
+    public class StateEnterMethodAttribute : Attribute
     {
         public string state;
 
-        public StateEnterMethod(string state) {
+        public StateEnterMethodAttribute(string state) {
             this.state = state;
         }
     }
 
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
-    public class StateExitMethod : System.Attribute
+    public class StateExitMethodAttribute : Attribute
     {
         public string state;
 
-        public StateExitMethod(string state) {
+        public StateExitMethodAttribute(string state) {
             this.state = state;
         }
-    }
-
-    public class StateMethod
-    {
-        public int stateHash;
-        public Action method;
     }
 }
